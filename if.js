4ec33099e79611d41187df6d3a290c3b00d175ac
Alejandro  Zambrano Lasso <alejandrozambranolasso@MacBook-Pro-de-Alejandro.local>
2022-08-27 12:48:45 -0500

    condiciones:if,else, else if

---
+++ if.js
@@ -0,0 +1,16 @@
+var edad = 18;
+
+if(edad === 18) {
+    console.log ("Puedes votar, será 1ra votación");
+} else if (edad > 18) {
+    console.log("Puedes votar de nuevo");
+} else {
+    console.log("Aun no puedes votar");
+}
+
+condition ? true : false;
+
+var numero = 1;
+
+var resultado = numero === 1 ? "si soy un uno" : "No, no soy un uno"
+
