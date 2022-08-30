b20798e08390ae96c0598015bb0f9b8dfc25a726
Alejandro  Zambrano Lasso <alejandrozambranolasso@MacBook-Pro-de-Alejandro.local>
2022-08-30 11:33:04 -0500

    Ejecicio de estudiantes

---
+++ arrays.js
@@ -0,0 +1,15 @@
+var frutas = ["Manzana", "Plátano", "Cereza","Fresa"];
+
+console.log(frutas);
+
+//
+var estudiantes = ["Maria", "Sergio", "Rosa", "Daniel"];
+
+function saludarEstudiantes(estudiantes){
+    console.log(`Hola, ${estudiantes}`);
+}
+
+for(var = i = 0; i < estudiantes.length; i++){
+    saludiarEstudiantes(estudiantes[i]);
+}
+//
