b1254da173f7e761d10370158790268873bf05c8
Alejandro  Zambrano Lasso <alejandrozambranolasso@MacBook-Pro-de-Alejandro.local>
2022-08-30 11:49:13 -0500

    ciclo for

--- arrays.js
+++ arrays.js
@@ -13,3 +13,15 @@ for(var = i = 0; i < estudiantes.length; i++){
     saludiarEstudiantes(estudiantes[i]);
 }
 //
+
+//|
+var estudiantes = ["Maria", "Sergio", "Rosa", "Daniel"];
+
+function saludarEstudiantes(estudiante){
+    console.log(`Hola, ${estudiante}`);
+}
+
+for(var estudiante of estudiantes){
+    saludarEstudiantes(estudiante);
+}
+//
