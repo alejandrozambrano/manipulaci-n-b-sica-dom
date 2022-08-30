0801cc684f16990675dc7bc27c4cdc1c8afaa4d1
Alejandro  Zambrano Lasso <alejandrozambranolasso@MacBook-Pro-de-Alejandro.local>
2022-08-30 12:54:50 -0500

    add console.log

--- arrays.js
+++ arrays.js
@@ -37,3 +37,17 @@ while(estudiantes.length > 0){
     var estudiante = estudiantes.shift();
     saludarEstudiantes(estudiante);
 //
+//
+var estudiantes = ["Maria", "Sergio", "Rosa", "Daniel"];
+
+function saludarEstudiantes (estudiante) {
+    console.log (`Hola, ${estudiante}`);
+}
+
+while(estudiantes.length > 0){
+    console.log(estudiantes);
+    var estudiante = estudiantes.shift();
+    saludarEstudiantes(estudiante);
+}
+
+//
