6fc175ca07f726a97061dbe7c62caa1b2e8d73d3
Alejandro  Zambrano Lasso <alejandrozambranolasso@MacBook-Pro-de-Alejandro.local>
2022-10-17 21:05:30 -0500

    Se añade addEventListener btn a js

--- ejercicio2/main.js
+++ ejercicio2/main.js
@@ -1,12 +1,24 @@
 const h1 = document.querySelector('h1');
+const form = document.querySelector('#form');
 const input1 = document.querySelector('#calculo1');
 const input2 = document.querySelector('#calculo2');
 const btn = document.querySelector('#btnCalcular');
 const result = document.querySelector('#result');
 
-btn.addEventListener('click', btnOnClick);
+// form.addEventListener('submit', sumarInputValues);
 
-function btnOnClick() {
-    const sumaInputs = input1.value + input2.value;
-    result.innerHTML = "Resultado: " + sumaInputs;
-  }
+// function sumarInputValues(event) {
+  // console.log({event});
+  // event.preventDefault();
+  // const sumaInputs = input1.value + input2.value;
+ //  result.innerText= "Resultado: " + sumaInputs;
+// }
+
+  btn.addEventListener('click', sumarInputValues);
+
+  function sumarInputValues(event) {
+  // console.log({event});
+  // event.preventDefault();
+   const sumaInputs = input1.value + input2.value;
+   result.innerText= "Resultado: " + sumaInputs;
+ }
